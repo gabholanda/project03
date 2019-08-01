9034212054c4191fc09574c936f0abdf6ae00a57
EuricoCruz <euricoscruz@hotmail.com>
2019-08-01 11:49:37 -0300

    fixed routes

--- API/app.js
+++ API/app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const bcrypt       = require('bcrypt');
 
 
 mongoose
@@ -53,5 +54,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const user = require('./routes/user')
+app.use('/auth', user)
 
 module.exports = app;
