1adc7923be4a30c0eda2f35cc84170c986168f7c
rfrancisco <renan.soares.francisco@gmail.com>
2019-08-03 12:49:16 -0300

    new router - movie by city and date

--- API/routes/movie.js
+++ API/routes/movie.js
@@ -5,43 +5,103 @@ const axios = require("axios");
 
 const router = express.Router();
 
-// GET route => Top Movies
+//GET route => Top Movies
 router.get("/destaques", (req, res, next) => {
   axios
     .get(
-      `https://api-content.ingresso.com/v0/templates/highlights/1?partnership=${INGRESSO_KEY}`
+      `https://api-content.ingresso.com/v0/templates/highlights/1?partnership=ironhackapp`
     )
-    .then(movie => res.json(movie.data))
-    .catch(err => console(err));
+    .then(movies => {
+      const movie = movies.data.map(movie => {
+        return {
+          id: movie.event.id,
+          title: movie.event.title,
+          image: movie.event.images[0].url
+        };
+      });
+      res.json(movie);
+    })
+    .catch(err => console.log(err));
 });
-
-router.get("/destaques/", (req, res, next) => {
-  axios
-    .get(
-      `https://api-content.ingresso.com/v0/templates/highlights/1?partnership=${INGRESSO_KEY}`
-    )
-    .then(movie => res.json(movie.data))
-    .catch(err => console(err));
-});
+//----------------------------------------------------------------------------------
 
 // GET route => On the cinema
 router.get("/cartaz", (req, res, next) => {
   axios
     .get(
-      `https://api-content.ingresso.com/v0/templates/nowplaying/1?partnership=${INGRESSO_KEY}`
+      `https://api-content.ingresso.com/v0/templates/nowplaying/1?partnership=ironhackapp`
     )
-    .then(movie => res.json(movie.data))
-    .catch(err => console(err));
+    .then(movies => {
+      const movie = movies.data.map(movie => {
+        return { id: movie.id, title: movie.title, image: movie.images[0].url };
+      });
+      res.json(movie);
+    })
+    .catch(err => console.log(err));
 });
+//------------------------------------------------------------------------------------
 
 // GET route => coming soon movies
-router.get("/em_breve", (req, res, next) => {
+router.get("/breve", (req, res, next) => {
   axios
     .get(
-      `https://api-content.ingresso.com/v0/templates/soon/1?partnership=${INGRESSO_KEY}`
+      `https://api-content.ingresso.com/v0/templates/soon/1?partnership=ironhackapp`
     )
-    .then(movie => res.json(movie.data))
-    .catch(err => console(err));
+    .then(movies => {
+      const movie = movies.data.map(movie => {
+        return { id: movie.id, title: movie.title, image: movie.images[0].url };
+      });
+      res.json(movie);
+    })
+    .catch(err => console.log(err));
 });
+//--------------------------------------------------------------------------------------
+
+// GET route => Movie by city and date
+router.get(
+  `/sessions/city/:cityId/event/:eventId/date/:date`,
+  (req, res, next) => {
+    const { cityId, eventId, date } = req.params;
+    axios
+      .get(
+        `https://api-content.ingresso.com/v0/sessions/city/${cityId}/event/${eventId}/partnership/ironhackapp?date=${date}`
+      )
+      .then(theaters => {
+        const theater = theaters.data[0].theaters.map(theater => {
+          return {
+            id: theater.id,
+            name: theater.name,
+            address: theater.address,
+            number: theater.number,
+            neighborhood: theater.neighborhood,
+            rooms: theater.rooms.map(room => {
+              return {
+                name: room.name,
+                sessions: room.sessions.map(session => {
+                  return {
+                    id: session.id,
+                    price: session.price,
+                    type: session.type,
+                    realDate: {
+                      dayOfWeek: session.realDate.dayOfWeek,
+                      dayAndMoth: session.realDate.dayAndMoth,
+                      hour: session.realDate.hour,
+                      year: session.realDate.year
+                    },
+                    siteURL: session.siteURL,
+                    enabled: session.enabled,
+                    blockMessage: session.blockMessage
+                  };
+                })
+              };
+            })
+          };
+        });
+        res.json(theater);
+      })
+      .catch(err => console.log(err));
+  }
+);
+//---------------------------------------------------------------------------------
 
 module.exports = router;
