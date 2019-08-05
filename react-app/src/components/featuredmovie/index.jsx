735dd19d6f9f44cce9172f159b268604db2f3edc
fredericonti <fred@conticonti.com>
2019-08-05 15:58:37 -0300

    featured movie

---
+++ react-app/src/components/featuredmovie/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FeaturedMovie.css'
+
+
+const FeaturedMovie = () => {
+  return (
+    <div>
+      <div className='featured-movie-bg'>
+        <img className='featured-image' src='../images/featured_movie.jpg' alt='movies' />
+
+        <div className='featured-info'>
+          <h1>Título do filme</h1>
+          <p>Descrição do filme e do evento em pelo menos 100 caracteres.</p>
+          <Link to='#' className='featured-link'>
+            <img className='search-icon' src='../images/play.svg' />
+            Veja mais
+          </Link>
+        </div>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default FeaturedMovie;
