79c4d61d3348baef8fb910622584a2afc6ca4454
rfrancisco <renan.soares.francisco@gmail.com>
2019-08-05 15:52:35 -0300

    Hamburger module

--- react-app/src/components/navbar/navbar.js
+++ react-app/src/components/navbar/navbar.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
-import HamburgerMenu from './hamburguer'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import HamburgerMenu from "./hamburger.jsx";
 class Navbar extends Component {
- constructor(props) {
-   super(props);
- }
- render() {
-   return (
-     <>
-       <div className='mobile-show'><HamburgerMenu /></div>
-       <div className='main-navbar'>
-         <Link to='#' className="navbar-brand ">
-           <img src="../images/logo.svg" alt="cinex logotype" />
-         </Link>
-         <div className='right-nav pc-show'>
-           <div className='search-bar'>
-             <img className='search-icon' src='../images/search_icon.svg' />
-             <input className='search-input'></input>
-           </div>
-           <div className='vertical-line'></div>
-           <Link to='#'>
-             <div className='avatar'></div>
-           </Link>
-           <Link to='#' className='login'>Login</Link>
-           <Link to='#' className='signup'>Signup</Link>
-         </div>
-       </div>
-     </>
-   )
- }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div className='mobile-show'>
+          <HamburgerMenu />
+        </div>
+        <div className='main-navbar'>
+          <Link to='/' className='navbar-brand '>
+            <img src='../images/logo.svg' alt='cinex logotype' />
+          </Link>
+          <div className='right-nav pc-show'>
+            <div className='search-bar'>
+              <img className='search-icon' src='../images/search_icon.svg' />
+              <input className='search-input' />
+            </div>
+            <div className='vertical-line' />
+            <Link to='#'>
+              <div className='avatar' />
+            </Link>
+            <Link to='/login' className='login'>
+              Login
+            </Link>
+            <Link to='/signup' className='signup'>
+              Signup
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 export default Navbar;
