67eb04adf2ca392a817a5d147056927f242910b0
Renan Soares Francisco <41347179+rfranciscos@users.noreply.github.com>
2019-08-05 15:14:13 -0300

    Update navbar.js

--- react-app/src/components/navbar/navbar.js
+++ react-app/src/components/navbar/navbar.js
@@ -1,54 +1,34 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
-
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+import HamburgerMenu from './hamburguer'
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <nav className='navbar navbar-expand-lg navbar-light navBody'>
-        <Link className='navbar-brand' to='/home'>
-          CineXP
-        </Link>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon' />
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <form className='form-inline my-2 my-lg-0'>
-            <input
-              className='form-control mr-sm-2'
-              type='search'
-              placeholder='Busque algum filme'
-              aria-label='Search'
-            />
-          </form>
-          <ul className='navbar-nav mr-auto'>
-            <li className='nav-item active signup-btn'>
-              <Link className='nav-link signup signup' to='/login'>
-                Login <span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/signup'>
-                Signup
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+ constructor(props) {
+   super(props);
+ }
+ render() {
+   return (
+     <>
+       <div className='mobile-show'><HamburgerMenu /></div>
+       <div className='main-navbar'>
+         <Link to='#' className="navbar-brand ">
+           <img src="../images/logo.svg" alt="cinex logotype" />
+         </Link>
+         <div className='right-nav pc-show'>
+           <div className='search-bar'>
+             <img className='search-icon' src='../images/search_icon.svg' />
+             <input className='search-input'></input>
+           </div>
+           <div className='vertical-line'></div>
+           <Link to='#'>
+             <div className='avatar'></div>
+           </Link>
+           <Link to='#' className='login'>Login</Link>
+           <Link to='#' className='signup'>Signup</Link>
+         </div>
+       </div>
+     </>
+   )
+ }
 }
-
 export default Navbar;
