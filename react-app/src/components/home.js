ed5a0ae524723d0f81834e105cfc4ef6c0f15ab1
rfrancisco <renan.soares.francisco@gmail.com>
2019-08-05 11:48:39 -0300

    Netflix Style

--- react-app/src/components/home.js
+++ react-app/src/components/home.js
@@ -7,8 +7,23 @@ class Movies extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { movies: [{ id: "", title: "", image: "" }], events: [] };
+    this.state = {
+      moviesHigh: [{ id: "", title: "", image: "" }],
+      movies: [{ id: "", title: "", image: "" }],
+      moviesSoon: [{ id: "", title: "", image: "" }]
+    };
   }
+
+  getMovieHigh = () => {
+    axios
+      .get(`http://localhost:5000/api/destaques`)
+      .then(responseFromApi => {
+        this.setState({
+          moviesHigh: responseFromApi.data
+        });
+      })
+      .catch(error => console.log(error));
+  };
 
   getMovie = () => {
     axios
@@ -21,22 +36,59 @@ class Movies extends Component {
       .catch(error => console.log(error));
   };
 
+  getMovieSoon = () => {
+    axios
+      .get(`http://localhost:5000/api/breve`)
+      .then(responseFromApi => {
+        this.setState({
+          moviesSoon: responseFromApi.data
+        });
+      })
+      .catch(error => console.log(error));
+  };
+
   componentDidMount() {
+    this.getMovieHigh();
     this.getMovie();
+    this.getMovieSoon();
   }
 
   render() {
     return (
       <div className=''>
-        <Slider>
-          {this.state.movies.map(movie => (
-            <Link to={`filme/${movie.id}`}>
-              <Slider.Item movie={movie} key={movie.id}>
-                item1
-              </Slider.Item>
-            </Link>
-          ))}
-        </Slider>
+        <div>
+          <Slider>
+            {this.state.moviesHigh.map(movie => (
+              <Link to={`filme/${movie.id}`}>
+                <Slider.Item movie={movie} key={movie.id}>
+                  item1
+                </Slider.Item>
+              </Link>
+            ))}
+          </Slider>
+        </div>
+        <div>
+          <Slider>
+            {this.state.movies.map(movie => (
+              <Link to={`filme/${movie.id}`}>
+                <Slider.Item movie={movie} key={movie.id}>
+                  item1
+                </Slider.Item>
+              </Link>
+            ))}
+          </Slider>
+        </div>
+        <div>
+          <Slider>
+            {this.state.moviesSoon.map(movie => (
+              <Link to={`filme/${movie.id}`}>
+                <Slider.Item movie={movie} key={movie.id}>
+                  item1
+                </Slider.Item>
+              </Link>
+            ))}
+          </Slider>
+        </div>
       </div>
     );
   }
