4abe641dbf1923b96dff8bc8641b04b2eef124ab
fredericonti <fred@conticonti.com>
2019-08-06 17:28:49 -0300

    login com css

--- react-app/src/components/auth/Login.js
+++ react-app/src/components/auth/Login.js
@@ -1,46 +1,43 @@
-import React, { Component } from "react";
-import AuthService from "./auth-service";
-import { Link } from "react-router-dom";
-import "./login.css";
-import Footer from "../footer/footer";
+  import React, { Component } from "react";
+  import AuthService from "./auth-service";
+  import { Link } from "react-router-dom";
+  import "./login.css";
+  import Footer from "../footer/footer";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: "", password: "" };
-    this.service = new AuthService();
-  }
+  class Login extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { username: "", password: "" };
+      this.service = new AuthService();
+    }
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    const username = this.state.username;
-    const password = this.state.password;
-    this.service
-      .login(username, password)
-      .then(response => {
-        this.setState({ username: "", password: "" });
-        this.props.getUser(response);
-      })
-      .catch(error => console.log(error));
-  };
+    handleFormSubmit = event => {
+      event.preventDefault();
+      const username = this.state.username;
+      const password = this.state.password;
+      this.service
+        .login(username, password)
+        .then(response => {
+          this.setState({ username: "", password: "" });
+          this.props.getUser(response);
+        })
+        .catch(error => console.log(error));
+    };
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+    handleChange = event => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+    };
 
-  render() {
-    return (
-      <div className='form-container'>
-        <div className='login-box'>
-          <Link className='login-form' onSubmit={this.handleFormSubmit}>
-            <Link to='/'>
-              <img className='back-home' src='./images/close.svg' />
-            </Link>
-            <h3 className='login-title'>
-              Entre agora na melhor de rede de eventos de cinema do Brasil
-            </h3>
-            ​
+    render() {
+      return (
+        <div className='form-container'>
+          <div className='login-box'>
+          <form className='login-form'onSubmit={this.handleFormSubmit}>
+          <Link to='/'><img className='back-home' src='./images/close.svg'/></Link>
+        
+          <h3 className='login-title'>Entre agora na melhor de rede de eventos de cinema do Brasil</h3>
+  
             <input
               type='text'
               name='username'
@@ -48,29 +45,32 @@ class Login extends Component {
               value={this.state.username}
               onChange={e => this.handleChange(e)}
             />
+  
             <input
               type='password'
               name='password'
               placeholder='Senha'
               value={this.state.password}
               onChange={e => this.handleChange(e)}
-            />
-            {/* button */}
+              />
+              {/* button */}
             <input type='submit' value='Entrar' />
-            <button className='signup-google'>
-              <img src='./images/google-logo.svg' />
-              <Link to='/evento/criar'>Entrar pelo Google</Link>
-            </button>
-            <hr className='login-div' />
+  
+          <button className='signup-google'>
+            <img src='./images/google-logo.svg'/>
+            <Link to='/evento/criar'>Entrar pelo Google</Link>
+          </button>
+  
+          <hr className='login-div'/>
+  
             <p className='login-cta'>Não possui conta?</p>
-            <Link className='signup-create' to='/evento/criar'>
-              Entre aqui seu cuzao
-            </Link>
-          </Link>
+            <Link className='signup-create'to='/evento/criar'>Entre aqui seu cuzao</Link>
+          </form>
+  
+          </div>
+          <Footer/>
         </div>
-        <Footer />
-      </div>
-    );
+      );
+    }
   }
-}
-export default Login;
+  export default Login;
