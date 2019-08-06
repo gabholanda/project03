096e929fe5fad9e35f95a409c2b3e503c484e0fb
rfrancisco <renan.soares.francisco@gmail.com>
2019-08-06 16:28:23 -0300

    Select options

--- react-app/src/components/createEvent/CreateEvent.jsx
+++ react-app/src/components/createEvent/CreateEvent.jsx
@@ -8,31 +8,101 @@ class EventMovie extends Component {
     super(props);
 
     this.state = {
-      movie: 0,
-      event: {
-        description: [
-          { interation: { image: "test", description: "test" }, _id: 0 }
-        ]
-      }
+      city: 1,
+      date: "",
+      event: [
+        {
+          id: "",
+          name: "",
+          address: "",
+          number: "",
+          neighborhood: "",
+          rooms: [
+            {
+              name: "",
+              sessions: [
+                {
+                  id: "",
+                  price: 0,
+                  type: [""],
+                  realDate: {
+                    dayOfWeek: "",
+                    hour: "",
+                    year: ""
+                  },
+                  siteURL: "",
+                  enabled: false,
+                  blockMessage: ""
+                }
+              ]
+            }
+          ]
+        }
+      ]
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleChangeDate = this.handleChangeDate.bind(this);
+    // this.getEvents = this.getEvents.bind(this);
   }
 
-  getEvents = () => {
+  getEvents(city, date) {
+    console.log(this.state.city);
+
+    // if (city === undefined && date === undefined) {
+    //   axios
+    //     .get(
+    //       `http://localhost:5000/api/sessions/city/1/event/${
+    //         this.props.match.params.movieId
+    //       }/date/${date}`
+    //     )
+    //     .then(responseFromApi => {
+    //       this.setState({
+    //         event: responseFromApi.data
+    //       });
+    //     })
+    //     .catch(error => console.log(error));
+    // } else {
     axios
-      .get(`http://localhost:5000/api/event/${this.props.match.params.eventId}`)
+      .get(
+        `http://localhost:5000/api/sessions/city/${city}/event/${
+          this.props.match.params.movieId
+        }/date/${date}`
+      )
       .then(responseFromApi => {
         this.setState({
           event: responseFromApi.data
         });
       })
       .catch(error => console.log(error));
-  };
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    if (event.target.name === "date") {
+      this.getEvents(this.state.city, event.target.value);
+    }
+  }
+
+  // handleChangeDate(event) {
+  //   this.setState({
+  //     date: event.target.value
+  //   });
+  //   this.getEvents(event.target.value);
+  // }
 
   componentDidMount() {
-    this.getEvents();
+    // this.getEvents();
   }
 
   render() {
+    console.log(this.state.event);
+
     return (
       <div className=''>
         <button type='submit'>Criar esse evento</button>
@@ -52,7 +122,49 @@ class EventMovie extends Component {
               id=''
               placeholder='Tipo de atividade'
             />
-            <input type='date' name='' id='' placeholder='Data do evento' />
+            //City
+            <select onChange={event => this.handleChange(event)} name='city'>
+              <option value='1'>São Paulo</option>
+              <option value='2'>Rio de Janeiro</option>
+              <option value='18'>Curitiba</option>
+              <option value='4'>Aracaju</option>
+              <option value='52'>Belém</option>
+              <option value='21'>Belo Horizonte</option>
+              <option value='441'>Boa Vista</option>
+              <option value='12'>Brasília</option>
+              <option value='47'>Campo Grande</option>
+              <option value='28'>Cuiabá</option>
+              <option value='68'>Florianópolis</option>
+              <option value='36'>Fortaleza</option>
+              <option value='15'>Goiânia</option>
+              <option value='32'>João Pessoa</option>
+              <option value='303'>Macapá</option>
+              <option value='53'>Maceió</option>
+              <option value='35'>Manaus</option>
+              <option value='48'>Natal</option>
+              <option value='363'>Palmas</option>
+              <option value='5'>Porto Alegre</option>
+              <option value='347'>Porto Velho</option>
+              <option value='22'>Recife</option>
+              <option value='364'>Rio Branco</option>
+              <option value='3'>Salvador</option>
+              <option value='19'>São Luís</option>
+              <option value='435'>Teresina</option>
+              <option value='11'>Vitória</option>
+            </select>
+            //Date
+            <input
+              type='date'
+              name='date'
+              id=''
+              onChange={event => this.handleChange(event)}
+            />
+            //Theater
+            <select>
+              {this.state.event.map(event => {
+                return <option value={event.id}>{event.name}</option>;
+              })}
+            </select>
             <input type='time' name='' id='' placeholder='Duração do evento' />
             <input
               type='text'
