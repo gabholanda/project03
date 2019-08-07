caaf6280ed45871ac22e17c63638cc59b129235f
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-08-07 11:34:55 -0300

    Updated models and Event Creation (backend)

--- API/routes/event.js
+++ API/routes/event.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Event = require("../models/Events");
-
+const User = require('../models/Users');
 const router = express.Router();
 
 // GET route => to get all the events
@@ -57,9 +57,8 @@ router.post("/events", (req, res, next) => {
     host,
     movieId
   } = req.body.st;
-  console.log(req.body.st);
 
-  Event.create({
+  const newEvent = new Event({
     title: eventTitle,
     duration: eventDuration,
     typeOfActivity: typeOfActivity,
@@ -77,12 +76,48 @@ router.post("/events", (req, res, next) => {
     host: host,
     movieId: movieId
   })
-    .then(response => {
-      console.log(response);
+
+  newEvent.save(err => {
+    if (err) {
+      res.status(400).json({ message: 'Error upon saving event on DB.' });
+      return;
+    }
+    // res.status(200).json(newEvent);
+  });
+  // User receives the event as it's host and event list
+  User.findByIdAndUpdate(req.user._id, { $push: { events: newEvent, host: newEvent } })
+    .then(user => {
+      // The event receives the event host as a member
+      Event.findOneAndUpdate({ host: user }, { $push: { members: user } })
+        .then(() => res.status(200).json({ message: 'Event created successfuly' }))
+        .catch(err => res.json(err))
     })
-    .catch(err => {
-      res.json(err);
-    });
+    .catch(err => res.status(400).json(err));
+
+  // Event.create({
+  //   title: eventTitle,
+  //   duration: eventDuration,
+  //   typeOfActivity: typeOfActivity,
+  //   language: language,
+  //   city: city,
+  //   date: date,
+  //   theaterId: theaterId,
+  //   session: sessionId,
+  //   firstInterationTitle,
+  //   firstInterationDescription,
+  //   secondInterationTitle,
+  //   secondInterationDescription,
+  //   thirdInterationTitle,
+  //   thirdInterationDescription,
+  //   host: host,
+  //   movieId: movieId
+  // })
+  //   .then(response => {
+  //     console.log(response);
+  //   })
+  //   .catch(err => {
+  //     res.json(err);
+  //   });
 });
 
 // GET route => to get a specific event/detailed view
