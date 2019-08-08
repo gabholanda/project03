169df0338c07768182c122308ac5534522319bf9
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-08-08 20:12:32 -0300

    Major bug fixes

--- react-app/src/components/eventMovie/EventMovie.jsx
+++ react-app/src/components/eventMovie/EventMovie.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "./EventMovie.css";
 import axios from "axios";
 import Footer from "../footer/footer";
@@ -9,6 +9,7 @@ class EventMovie extends Component {
     super(props);
 
     this.state = {
+      redirect: false,
       posterV: "",
       movie: 0,
       event: {
@@ -84,7 +85,9 @@ class EventMovie extends Component {
     axios
       .put(`http://localhost:5000/api/join-event/${this.props.eventId}/user/${this.props.user._id}`)
       .then(responseFromApi => {
-        console.log(responseFromApi)
+        this.setState({
+          redirect: true
+        })
       })
       .catch(error => console.log(error));
   }
@@ -93,110 +96,113 @@ class EventMovie extends Component {
   }
 
   render() {
-    console.log(this.state.event.host)
-    return (
-      <>
-        <nav aria-label='breadcrumb'>
-          <ol className='breadcrumb'>
-            <li className='breadcrumb-item'>
-              <Link to='/'>Início</Link>
+    if (this.state.redirect) {
+      return <Redirect to='/' />;
+    } else {
+      return (
+        <>
+          <nav aria-label='breadcrumb'>
+            <ol className='breadcrumb'>
+              <li className='breadcrumb-item'>
+                <Link to='/'>Início</Link>
+              </li>
+              <li className='breadcrumb-item'>
+                <Link to={`/filme/${this.state.event.movieId}`}>Filme</Link>
+              </li>
+              <li className='breadcrumb-item active' aria-current='page'>
+                Evento
             </li>
-            <li className='breadcrumb-item'>
-              <Link to={`/filme/${this.state.event.movieId}`}>Filme</Link>
-            </li>
-            <li className='breadcrumb-item active' aria-current='page'>
-              Evento
-            </li>
-          </ol>
-        </nav>
-        <div className='eventPage'>
-          <div className='eventPage-left'>
-            {/* image */}
-            <img className='event-poster' src={this.state.posterV} alt='' />
-            {/* buttons */}
-            <div>
-              <button className='event-buttons'>
-                <Link to='www.google.com.br'>Editar evento</Link>
-              </button>
-              <button className='event-buttons-exit '>
-                <Link to='www.google.com.br'>Sair do grupo</Link>
+            </ol>
+          </nav>
+          <div className='eventPage'>
+            <div className='eventPage-left'>
+              {/* image */}
+              <img className='event-poster' src={this.state.posterV} alt='' />
+              {/* buttons */}
+              <div>
+                <button className='event-buttons'>
+                  <Link to='www.google.com.br'>Editar evento</Link>
+                </button>
+                <button className='event-buttons-exit '>
+                  <Link to='www.google.com.br'>Sair do grupo</Link>
+                </button>
+              </div>
+            </div>
+
+            <div className='eventPage-right'>
+
+              <h3 className='event-activity'>Atividade{this.state.event.typeOfActivity}</h3>
+              <h1 className='event-title'>titulo{this.state.event.title}</h1>
+              <p className='event-date'>ddfgdfg{this.state.event.dateMovie}</p>
+              <hr className='event-div' />
+              <div className='event-important-info'>
+                <p className=''>
+                  O evento tem duração de<br /> {this.state.event.duration} minutos
+              </p>
+                <p className=''>o evento será falado em <br />{this.state.event.language}</p>
+                <p className=''>
+                  {this.state.event.participants} pessoas <br />irão a esse evento
+              </p>
+              </div>
+
+              {/* O que iremos fazer */}
+              <div>
+                <h2 className='event-todo-title'>O que vamos fazer?</h2>
+
+                <div className='interation'>
+                  {/* <img src='' alt='' /> */}
+                  <h4>1. {this.state.event.firstInterationTitle}</h4>
+                  <p>{this.state.event.firstInterationDescription}</p>
+                </div>
+                <div className='interation'>
+                  {/* <img src='' alt='' /> */}
+                  <h4>2. {this.state.event.secondInterationTitle}</h4>
+                  <p>{this.state.event.secondInterationDescription}</p>
+                </div>
+                <div className='interation'>
+                  {/* <img src='' alt='' /> */}
+                  <h4>3. {this.state.event.thirdInterationTitle}</h4>
+                  <p>{this.state.event.thirdInterationDescription}</p>
+                </div>
+              </div>
+
+              {/* maps */}
+              <div className='event-maps-container'>
+                <h2 className='event-maps-title'>Local do Filme</h2>
+                <div className='maps'>
+                  <img src='../images/maps.png' alt='cinex logotype' />
+                </div>
+                {/* // GOOGLE MAPS HERE */}
+              </div>
+
+              {/* host */}
+              <h2 className='event-host-title'>Host do Evento</h2>
+
+              <div className='event-host'>
+                <div className='host-image'>
+                  <img src='' alt='host image here please' />
+                </div>
+
+
+                <div className='host-div'>
+                  <div className='host-info'>
+                    <h4>{this.state.event.host.name}</h4>
+                    <p>{this.state.event.host.name}</p>
+                  </div>
+                </div>
+              </div>
+
+              <button className='event-button-exit2' onClick={() => this.enterEvent()}>
+                Participar!
               </button>
             </div>
           </div>
 
-        <div className='eventPage-right'>
-
-            <h3 className='event-activity'>Atividade{this.state.event.typeOfActivity}</h3>
-            <h1 className='event-title'>titulo{this.state.event.title}</h1>
-            <p className='event-date'>ddfgdfg{this.state.event.dateMovie}</p>
-            <hr className='event-div'/>
-            <div className='event-important-info'>
-              <p className=''>
-                O evento tem duração de<br/> {this.state.event.duration} minutos
-              </p>
-              <p className=''>o evento será falado em <br/>{this.state.event.language}</p>
-              <p className=''>
-                {this.state.event.participants} pessoas <br/>irão a esse evento
-              </p>
-            </div>
-
-            {/* O que iremos fazer */}
-            <div>
-              <h2 className='event-todo-title'>O que vamos fazer?</h2>
-
-              <div className='interation'>
-                {/* <img src='' alt='' /> */}
-                <h4>1. {this.state.event.firstInterationTitle}</h4>
-                <p>{this.state.event.firstInterationDescription}</p>
-              </div>
-              <div className='interation'>
-                {/* <img src='' alt='' /> */}
-                <h4>2. {this.state.event.secondInterationTitle}</h4>
-                <p>{this.state.event.secondInterationDescription}</p>
-              </div>
-              <div className='interation'>
-                {/* <img src='' alt='' /> */}
-                <h4>3. {this.state.event.thirdInterationTitle}</h4>
-                <p>{this.state.event.thirdInterationDescription}</p>
-              </div>
-            </div>
-
-            {/* maps */}
-            <div className='event-maps-container'>
-              <h2 className='event-maps-title'>Local do Filme</h2>
-              <div className='maps'>
-                <img src='../images/maps.png' alt='cinex logotype' />
-              </div>
-              {/* // GOOGLE MAPS HERE */}
-            </div>
-
-            {/* host */}
-            <h2 className='event-host-title'>Host do Evento</h2>
-
-              <div className='event-host'>
-              <div className='host-image'>
-                <img src='' alt='host image here please' />
-              </div>
-            
-
-              <div className='host-div'>
-                <div className='host-info'>
-                  <h4>{this.state.event.host.name}</h4>
-                  <p>{this.state.event.host.name}</p>
-                </div>
-              </div>
-            </div>
-
-            <button className='event-button-exit2'>
-              <Link to='www.google.com.br' onClick={() => this.enterEvent()}>Participar</Link>
-            </button>
-        </div>
-        </div>
-
           <Footer />
         </>
-          );
-        }
-      }
-      export default EventMovie;
+      );
+    }
+  }
+}
+export default EventMovie;
 
